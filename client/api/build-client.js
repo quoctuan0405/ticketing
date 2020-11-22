983c159ac7d21a4c115d73717dd95e90f6e676c3
Your Name <you@example.com>
2020-11-22 16:28:12 +0700

    Fixing ingress nginx

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,11 +3,18 @@ import axios from 'axios';
 const buildRequest = ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server
-
+    if (process.env.NODE_ENV === 'development'|| process.env.NODE_ENV === 'test') {
+      return axios.create({
+        baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+        headers: req.headers
+      });
+    }
+    
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'https://tuan-ticketing-app.xyz/',
       headers: req.headers
     });
+
   } else {
     // We must be on the browser
     return axios.create({
